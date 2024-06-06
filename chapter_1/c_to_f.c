78732d709dc65773dcce3b2409a132f6ba530336
Arman Jasuja <arman_jasuja@yahoo.com>
2024-06-06 22:59:26 +0400

    Spacing fix

--- chapter_1/c_to_f.c
+++ chapter_1/c_to_f.c
@@ -3,10 +3,10 @@
 int main(void)
 {
     float fahr;
-    printf("%3c%6c\n", 'f', 'c');
+    printf("%3c%7c\n", 'f', 'c');
     for (float cels = 0; cels <= 148.9; cels += 11.1)
     {
         fahr = ((9.0 / 5.0) * cels) + 32.0;
-        printf("%4.1f%6.0f\n", cels, fahr);
+        printf("%5.1f%6.0f\n", cels, fahr);
     }
 }
