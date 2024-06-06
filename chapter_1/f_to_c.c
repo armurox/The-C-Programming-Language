81964465ba1c01220b267e60db9cbfa6310736d8
Arman Jasuja <arman_jasuja@yahoo.com>
2024-06-06 23:01:40 +0400

    Removed unecessary variable

--- chapter_1/f_to_c.c
+++ chapter_1/f_to_c.c
@@ -2,11 +2,9 @@
 
 int main(void)
 {
-    float cels;
     printf("%3c%10c\n", 'c', 'f');
     for (float fahr = 0; fahr <= 300; fahr += 20)
     {
-        cels = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f\t%6.1f\n", fahr, cels);
+        printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
     }
 }
