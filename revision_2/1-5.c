e234bde1ed3ca1c486221c6c2ae55fa803972e97
Arman Jasuja <arman_jasuja@yahoo.com>
2025-03-30 12:59:46 +0400

    Exercise 1-5 completed

---
+++ revision_2/1-5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MIN_F 0
+#define MAX_F 300
+#define STEP_SIZE 20
+
+int main(void)
+{
+    printf("%3c %5c\n", 'F', 'C');
+    for (float f = MAX_F; f >= MIN_F; f -= STEP_SIZE)
+    {
+        printf("%3.0f %6.1f\n", f, (5.0 * (f - 32.0)) / 9.0);
+    }
+
+}
