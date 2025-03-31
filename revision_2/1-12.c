7d64ba59176b3f29e6affe546a07b8382b62ee5e
Arman Jasuja <arman_jasuja@yahoo.com>
2025-03-31 12:29:06 +0400

    Exercise 1-12 solution added

---
+++ revision_2/1-12.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 2
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./wc [FILENAME]\n");
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        printf("Could not find / open file %s!\n", argv[1]);
+        return 2;
+    }
+
+    int state = OUT;
+    int c;
+    while ((c = fgetc(f)) != EOF)
+    {
+        if ((c == ' ' || c == '\n' || c == '\t') && state == IN)
+        {
+            state = OUT;
+            putchar('\n');
+        }
+
+        else if ((c != ' ' && c != '\n' && c != '\t'))
+        {
+            state = IN;
+            putchar(c);
+        }
+    }
+
+}
