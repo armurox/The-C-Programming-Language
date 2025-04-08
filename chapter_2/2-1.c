179bc6f168d150300de778d120d2afde1659ed1f
Arman Jasuja <arman_jasuja@yahoo.com>
2025-04-08 22:30:32 +0400

    Added everything except floating point types

--- chapter_2/2-1.c
+++ chapter_2/2-1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -6,19 +7,34 @@ int main(void)
     unsigned char uc = 0;
     unsigned char prev_uc = uc;
     signed char sc;
-    unsigned int ui;
+    unsigned int ui = 0;
+    unsigned int prev_ui = ui;
     signed int si;
-    unsigned short usi;
+    unsigned short usi = 0;
+    unsigned int prev_usi = usi;
     signed short ssi;
-    unsigned long uli;
+    unsigned long uli = 0;
+    unsigned long prev_uli = uli;
     signed long sli;
+    float f = 0;
     for (c = 0; c >= 0; c++)
         ;
     for (uc = 0; uc >= prev_uc; uc++)
         prev_uc = uc;
     for (sc = 0; sc >= 0; sc++)
         ;
+    for (ui = 0; ui >= prev_ui; ui++)
+        prev_ui = ui;
+    for (usi = 0; usi >= prev_usi; usi++)
+        prev_usi = usi;
+    for (si = 0; si >= 0; si++)
+        ;
     printf("char range is: %d to %d\n", c, (char) (c - 1));
     printf("unsigned char range is: %d to %d\n", (unsigned char) (uc), (unsigned char) prev_uc);
-    printf("unsigned char range is: %d to %d\n", sc, (unsigned char) (sc - 1));
+    printf("signed char range is: %d to %d\n", sc, (unsigned char) (sc - 1));
+    printf("unsigned int range is: %u to %u\n", ui, prev_ui);
+    printf("unsigned short range is: %u to %u\n", usi, prev_usi);
+    printf("signed int range is: %d to %d\n", si, si - 1);
+    printf("unsigned long range is: %lu to %lu\n", (unsigned long) 0, (unsigned long) pow(2, (8 * sizeof(unsigned long))));  // To avoid a very long runtime
+
 }
